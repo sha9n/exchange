c0f8d6af2836aa70cdc8fecbc446dc0828bcb443
Stas Yaprintsev <21stason@gmail.com>
2023-03-22 18:36:10 +0300

    Убрал =, автоматическая прогрузка валюты

--- src/index.js
+++ src/index.js
@@ -2,48 +2,35 @@ const apiUrl = "https://www.cbr-xml-daily.ru/daily_json.js";
 
 const inputCurrency = document.getElementById("inputCurrency");
 const currencyList = document.getElementById("currencyList");
-const button = document.getElementById("button");
 const outputCurrency = document.getElementById("outputCurrency");
 
 fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    const currencies = data.Valute;
-
-    for (const [key, value] of Object.entries(currencies)) {
-      const option = document.createElement("option");
-      option.value = key;
-      option.textContent = `${key} (${value.Name})`;
-      currencyList.appendChild(option);
-    }
-  });
-
-button.addEventListener("click", () => {
-  const selectedCurrency = currencyList.value;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const rate = data.Valute[selectedCurrency].Value;
-
-      const inputAmount = parseFloat(inputCurrency.value);
-      if (!isNaN(inputAmount)) {
-        outputCurrency.value = (inputAmount * rate).toFixed(2);
-      }
-    });
-});
-
-currencyList.addEventListener("change", () => {
-  const selectedCurrency = currencyList.value;
-  if (inputCurrency.value !== "") {
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        const rate = data.Valute[selectedCurrency].Value;
-        const inputAmount = parseFloat(inputCurrency.value);
-        if (!isNaN(inputAmount)) {
-          outputCurrency.value = (inputAmount * rate).toFixed(2);
-        }
-      });
+.then((response) => response.json())
+.then((data) => {
+const currencies = data.Valute;
+for (const [key, value] of Object.entries(currencies)) {
+    const option = document.createElement("option");
+    option.value = key;
+    option.textContent = `${key} (${value.Name})`;
+    currencyList.appendChild(option);
   }
 });
+
+function convertCurrency() {
+const selectedCurrency = currencyList.value;
+if (inputCurrency.value !== "") {
+fetch(apiUrl)
+.then((response) => response.json())
+.then((data) => {
+const rate = data.Valute[selectedCurrency].Value;
+const inputAmount = parseFloat(inputCurrency.value);
+if (!isNaN(inputAmount)) {
+outputCurrency.value = (inputAmount * rate).toFixed(2);
+}
+});
+}
+}
+
+inputCurrency.addEventListener("input", convertCurrency);
+currencyList.addEventListener("change", convertCurrency);
+
