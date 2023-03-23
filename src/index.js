1eeb64fa0fe276cc6cb736b95a6093d710fc727a
Stas Yaprintsev <21stason@gmail.com>
2023-03-23 17:06:51 +0300

    Добавил defer

--- src/index.js
+++ src/index.js
@@ -5,32 +5,31 @@ const currencyList = document.getElementById("currencyList");
 const outputCurrency = document.getElementById("outputCurrency");
 
 fetch(apiUrl)
-.then((response) => response.json())
-.then((data) => {
-const currencies = data.Valute;
-for (const [key, value] of Object.entries(currencies)) {
-    const option = document.createElement("option");
-    option.value = key;
-    option.textContent = `${key} (${value.Name})`;
-    currencyList.appendChild(option);
-  }
-});
+  .then((response) => response.json())
+  .then((data) => {
+    const currencies = data.Valute;
+    for (const [key, value] of Object.entries(currencies)) {
+      const option = document.createElement("option");
+      option.value = key;
+      option.textContent = `${key} (${value.Name})`;
+      currencyList.appendChild(option);
+    }
+  });
 
 function convertCurrency() {
-const selectedCurrency = currencyList.value;
-if (inputCurrency.value !== "") {
-fetch(apiUrl)
-.then((response) => response.json())
-.then((data) => {
-const rate = data.Valute[selectedCurrency].Value;
-const inputAmount = parseFloat(inputCurrency.value);
-if (!isNaN(inputAmount)) {
-outputCurrency.value = (inputAmount * rate).toFixed(2);
-}
-});
-}
+  const selectedCurrency = currencyList.value;
+  if (inputCurrency.value !== "") {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const rate = data.Valute[selectedCurrency].Value;
+        const inputAmount = parseFloat(inputCurrency.value);
+        if (!isNaN(inputAmount)) {
+          outputCurrency.value = (inputAmount * rate).toFixed(2);
+        }
+      });
+  }
 }
 
 inputCurrency.addEventListener("input", convertCurrency);
 currencyList.addEventListener("change", convertCurrency);
-
